0a4a6e55ec1cafe7d638f7d0bb97bfa414e97284
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2022-02-15 15:25:16 -0600

    ToDo General structure and first lines of js

---
+++ week06/script.js
@@ -0,0 +1,27 @@
+
+window.onload = loadToDoList() {
+    if (!localStorage.toDoList){
+        var toDoList = {};
+    }
+}
+
+console.log("Hello");
+
+
+
+
+document.getElementById('add-btn').addEventListener('click', addElement);
+
+// function addElement {
+
+//     let newtask = document.getElementById('newtask').value;
+    
+//     tasks.apend(newtask)
+//     // Save
+//     var tasks = ["1", "2", "3"];
+//     localStorage["datas"] = JSON.stringify(datas);
+
+//     // Retrieve
+//     var stored_datas = JSON.parse(localStorage["datas"]);
+// }
+
