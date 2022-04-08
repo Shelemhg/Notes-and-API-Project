6544360e9f6ea40f5d3f9eaa876957c48f7a1ca3
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2022-04-07 20:09:48 -0500

    Finished Graph plotting

--- final project/app.js
+++ final project/app.js
@@ -5,12 +5,14 @@ import {displayMessage} from "./products.js";
 import {renderProducts} from "./products.js";
 import {getAnsi} from "./products.js";
 import {getInfo} from "./products.js";
+import {addPriceHistory} from "./products.js";
 
 var Products = [];
 
 window.addEventListener("load", () => {
     Products = loadProductList();    
     renderProducts(Products);
+    // console.log(JSON.stringify(Products));
 } );
 
 function changeColor(color){
@@ -21,6 +23,8 @@ function changeColor(color){
     document.getElementById('message').classList.add(color);
     return false;
 }
+
+
 
 export async function addNewProduct(){
     changeColor('white');
@@ -34,6 +38,7 @@ export async function addNewProduct(){
         var ansi = getAnsi(productUrl);
         changeColor('yellow');
         displayMessage("Retriving information");
+
         // SEND REQUEST
         const product = await getInfo(ansi);
 
@@ -41,7 +46,6 @@ export async function addNewProduct(){
             changeColor('red');
             displayMessage("Error finding item. Please check the URL or try a different one.");
         }else{
-            console.log("Saving new product");
             // SAVE product
             const newProduct = new Product(
                 product.product_id, 
@@ -50,18 +54,55 @@ export async function addNewProduct(){
                 product.product_main_image_url,
                 product.available_quantity,
                 `https://www.amazon.com/gp/product/${ansi}/`,
-                document.getElementById("comment").value);       
+                document.getElementById("comment").value); 
+            
+            function searchANSI(){
+                find: {
+                    for(let i=0; i<Products.length; i++){
+                        if(newProduct.id == Products[i].id){
+                            addPriceHistory(newProduct, i, Products);
+                            break find;
+                        }
+                    }
+                    saveProduct(newProduct, Products);
+                }
+            }
+
+            searchANSI();
+                
             document.getElementById("productUrl").value = "";
-            document.getElementById("comment").value = "";     
-            saveProduct(newProduct, Products);
+            document.getElementById("comment").value = "";
+
             changeColor('green');
             displayMessage("Product added succesfully");
-        }  
+        } 
         Products = loadProductList();
     }else{
         changeColor('red');
         displayMessage("Please enter a valid URL");
     }
+
+
+    
+
+    // console.log("Saving new product");
+    // // SAVE product
+    // const newProduct = new Product(
+    //     "111", 
+    //     "Title test 1", 
+    //     3.99,
+    //     "URL test 1",
+    //     11,
+    //     `https://www.amazon.com`,
+    //     "ola ke mira");
+
+
+
+    
+
+    // console.log("addition to array:");
+    // console.log(JSON.stringify(newProduct.price_history));
+
     renderProducts(Products); // Display Products on Screen
     return false;
 }
